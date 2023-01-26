c4c2207406c899af3fed93eb961186f46a3edbdc
nuril-nuzula <nurilnuzula222@gmail.com>
2023-01-26 11:04:03 +0700

    a

--- OOP/destructor.php
+++ OOP/destructor.php
@@ -1,10 +1,14 @@
 <?php
 class Connection
 {
+// X. constructor dan Destructor
+// 83
 public function __destruct()
 {
 echo 'Object dihapus dari memory';
 }
 }
+
 $connection = new Connection();
+$connection->__destruct();
 unset($connection);
